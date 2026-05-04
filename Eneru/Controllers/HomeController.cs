fb3a4ac322208776698807db87aaee5cd07de013
enelthegod <tkachenkonazar2@gmail.com>
2026-05-04 16:42:30 +0100

    style: update home page with hero section and redesign order pages

--- Eneru/Controllers/HomeController.cs
+++ Eneru/Controllers/HomeController.cs
@@ -1,25 +1,30 @@
-using Eneru.Models;
+using Eneru.Data;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eneru.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+
+        public HomeController(AppDbContext db)
         {
-            return View();
+            _db = db;
         }
 
-        public IActionResult Privacy()
+        // GET /
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            // Load 6 newest available products for the featured section
+            var featuredProducts = await _db.Products
+                .Include(p => p.Category)
+                .Where(p => p.IsAvailable)
+                .OrderByDescending(p => p.CreatedAt)
+                .Take(6)
+                .ToListAsync();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(featuredProducts);
         }
     }
 }
